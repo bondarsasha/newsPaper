4fb64894dc159e2f80349bfbbe81bd7ff8a54148
bondarsasha <bondaralexandr1991@gmail.com>
2017-11-20 21:17:28 +0200

    changeOnLesson

--- js/user.js
+++ js/user.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", ready);
 function ready() {
-
+//button
       var down = document.getElementById('nav-trigger');
 
       down.addEventListener("click", function() {
@@ -12,16 +12,16 @@ function ready() {
           }
       });
 
-
+//slider
       var imgArr = ['1.jpg', '2.jpg', '3.jpg'];
       var slider = document.getElementById('images');
       slider.src = "img/" + imgArr[0];
 
-      var right = document.getElementById('rightBtn');
-      var left = document.getElementById('leftBtn');
+      var prevBtn = document.getElementById('prevBtn');
+      var nextBtn = document.getElementById('nextBtn');
 
-      // right.addEventListener("click", next, false);
-      // left.addEventListener("click", prev, false);
+      nextBtn.addEventListener("click", next, false);
+      prevBtn.addEventListener("click", prev, false);
 
       var i=0;
 
@@ -41,4 +41,49 @@ function ready() {
             slider.src = "img/" + imgArr[i];
       }
       setInterval( next, 2000 );
+
+      //sign-modal
+
+          var sign = document.getElementById('sign_in');
+
+          sign.addEventListener("click", function() {
+            var showModal = document.getElementById('modal_sign');
+              if (showModal.style.display == 'none') {
+                showModal.style.display = 'block';
+              } else {
+                showModal.style.display = 'none';
+              }
+          });
+
+          //button in sign_form
+          var close = document.getElementById('close');
+
+          close.addEventListener("click", function() {
+            var showModal = document.getElementById('modal_sign');
+              if (showModal.style.display == 'block') {
+                showModal.style.display = 'none';
+              } else {
+                showModal.style.display = 'block';
+              }
+          });
+
+          var modalClose = document.getElementById('modal_sign')
+
+            modalClose.addEventListener("click", function(e) {
+              if (e.target.id === 'modal_sign') {
+                modalClose.style.display = 'none';
+              }
+            })
+
+
+          //get
+          $.get("https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=6f7c85381a5c44deb7e024cd02c60e31", function(articles) {
+            var newsArr = articles;
+
+            for(key in newsArr.articles) {
+              console.log(newsArr.articles[key]);
+            }
+          });
+
+
 }
